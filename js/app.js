1055ad562fc15ac4a9ee24a6aa69eeef0f9900c0
Mauro Coniglio <mauroconiglioo48@gmail.com>
2024-08-27 10:09:58 -0300

    di funcionalidad al boton modal desde javascript

--- js/app.js
+++ js/app.js
@@ -2,8 +2,18 @@ import Servicios from "./classServicio.js";
 
  //aqui agregare la logica del CRUD
 
-console.log('anda mi codigo loco')
+ //declaro las variables
+
 const servicios = new Servicios('Reels y Videos','sdofgnsdfkjg','500','5 horas');
 console.log(servicios)
+const modalContacto = new bootstrap.Modal (document.getElementById('modalAdminServicios'));
+const btnNuevo = document.getElementById('btnNuevo');
+
+//funciones
+const mostrarModal = ()=>{
+    modalContacto.show()
+}
 
 
+//aqui agrego la logica del CRUD
+btnNuevo.addEventListener('click', mostrarModal)
