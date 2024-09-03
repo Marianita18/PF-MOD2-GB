a304a166fa32116841665b0b5811da70febe4333
eze <ezequiel@rodrigoinfante.com.ar>
2024-09-03 00:01:23 -0300

    nav perdida del admin

--- js/registro.js
+++ js/registro.js
@@ -1,13 +1,14 @@
 import Usuario from "./classUsuario.js";
-const usuario = document.getElementById("usuario")
-const nombre = document.getElementById("nombre")
-const apellido = document.getElementById("apellido")
-const fechaNacimiento = document.getElementById("fechaNacimiento")
-const telefono = document.getElementById("telefono")
-const email = document.getElementById("email")
-const password = document.getElementById("password")
-const tipo = document.getElementById("tipo")
-const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+const usuario = document.getElementById("usuario");
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido");
+const fechaNacimiento = document.getElementById("fechaNacimiento");
+const telefono = document.getElementById("telefono");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const tipo = document.getElementById("tipo");
+const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 const btncargar = document.getElementById("btncargar");
 
 const limpiarFormulario = () => {
@@ -19,31 +20,26 @@ const limpiarFormulario = () => {
   document.getElementById("email").value = "";
   document.getElementById("password").value = "";
   document.getElementById("tipo").value = "";
-
 };
-document.getElementById("btn-nuevousario").addEventListener("click", function () {
-  const modalUsuario = new bootstrap.Modal(document.getElementById("modalnuevousario"));
-  modalUsuario.show();
-});
+
+document
+  .getElementById("btn-nuevousario")
+  .addEventListener("click", function () {
+    const modalUsuario = new bootstrap.Modal(
+      document.getElementById("modalnuevousario")
+    );
+    modalUsuario.show();
+  });
 
 const Admin = (usuario) => {
-  return (
-    usuario.nombre === "admin" &&
-    usuario.apellido === "admin" &&
-    usuario.email === "admin@gmail.com"&&
-    usuario.password === "admin"
-  );
+  return usuario.email.trim() === "admin@gmail.com" && usuario.password.trim() === "admin";
 };
 
 const obtenerUsuario = () => {
-  const nombre = document.getElementById("nombre1").value;
-  const apellido = document.getElementById("apellido1").value;
-  const email = document.getElementById("email1").value;
-  const password = document.getElementById("contrasena").value;
-  
-  return { nombre, apellido, email, password };
+  const email = document.getElementById("email1").value.trim();
+  const password = document.getElementById("contrasena").value.trim();
+  return { email, password };
 };
-
 
 const crearUsuario = () => {
   const nusuario = new Usuario(
@@ -54,7 +50,7 @@ const crearUsuario = () => {
     telefono.value,
     email.value,
     password.value,
-    tipo.value,
+    tipo.value
   );
   guardarEnLocalStorage(nusuario);
   limpiarFormulario();
@@ -65,32 +61,35 @@ const guardarEnLocalStorage = (nusuario) => {
   localStorage.setItem("usuarios", JSON.stringify(usuarios));
   console.log("Usuario guardado en el local storage");
 };
-document.getElementById("form-nuevousario").addEventListener("submit", crearUsuario);
+
+document
+  .getElementById("form-nuevousario")
+  .addEventListener("submit", crearUsuario);
+
 const cargarUsuario = (usuario) => {
-  document.getElementById("nombre1").value = usuario.nombre;
-  document.getElementById("apellido1").value = usuario.apellido;
   document.getElementById("email1").value = usuario.email;
-  document.getElementById("nacimiento1").value = usuario.fechaNacimiento;
-  document.getElementById("telefono").value = usuario.telefono;
-  document.getElementById("telefoncargar").value = usuario.password;
-  
+  document.getElementById("contrasena").value = usuario.password;
 };
 
 btncargar.addEventListener("click", () => {
   const usuario = obtenerUsuario();
   if (Admin(usuario)) {
     console.log("El usuario es administrador");
-    document.getElementById("admin-nav").style.display = "block"; // Mostrar el elemento del administrador
+    window.location.href = "../index.html";
   } else {
-    const usuarioEncontrado = usuarios.find(u => u.email === usuario.email && u.password === usuario.password);
+    const usuarioEncontrado = usuarios.find(
+      (u) => u.email === usuario.email && u.password === usuario.password
+    );
     if (usuarioEncontrado) {
       console.log("Usuario encontrado en el local storage");
       cargarUsuario(usuarioEncontrado);
-      window.location.href = "../index.html";
     } else {
-      const modalUsuario = new bootstrap.Modal(document.getElementById("modalnuevousario"));
-      console.log("Usuario no encontrado, mostrando formulario de creación");
+      const modalUsuario = new bootstrap.Modal(
+        document.getElementById("modalnuevousario")
+      );
       modalUsuario.show();
+      console.log("Usuario no encontrado, mostrando formulario de creación");
+      document.getElementById("admin-nav").style.display = "block";
     }
   }
 });
